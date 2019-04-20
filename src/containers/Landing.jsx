2c1fa7835bb23e576233af7c3f96fe5aaf97ad97
abhay-vaidya <abhay6547@gmail.com>
2019-04-20 19:28:43 -0400

    Design tweaks

--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -6,14 +6,17 @@ class Landing extends Component {
     return (
       <section
         id="landing"
-        className="flex flex-col sm:flex-row w-full h-screen items-center justify-center p-8"
+        className="flex flex-col md:flex-row w-full h-screen items-center justify-center p-8"
       >
-        <div className="w-full mt-24 md:mt-0 sm:w-1/2">
+        <div className="w-full mt-24 md:mt-0 md:w-1/3">
           <h1 className="pb-6 text-5xl gradient-bg">Hi, I'm Abhay.</h1>
-          <p>I study Computer Science at the University of Toronto.</p>
+          <p>
+            I'm a third-year Computer Science student at the University of
+            Toronto.
+          </p>
         </div>
-        <div className="w-full sm:w-1/2">
-          <img className="self-end mt-6" src={profile} alt="Abhay" />
+        <div className="w-full md:w-2/3">
+          <img className="md:ml-6" src={profile} alt="Abhay" />
         </div>
       </section>
     )
--- src/containers/Skills.jsx
+++ src/containers/Skills.jsx
@@ -6,7 +6,7 @@ class Skills extends Component {
     return (
       <Section title="Skills" id="skills">
         <div className="flex flex-col md:flex-row justify-end mb-6">
-          <h1 className="gradient-bg mb-4 md:mr-4 md:mt-3">
+          <h1 className="gradient-bg mb-4 md:mr-4 md:mt-2">
             I design cool things in
           </h1>
           <ul className="list-reset flex flex-col text-2xl py-3 px-4 border-grey-darkest border-solid border-2">
@@ -16,7 +16,7 @@ class Skills extends Component {
           </ul>
         </div>
         <div className="flex flex-col md:flex-row justify-end">
-          <h1 className="gradient-bg mb-4 md:mr-4 md:mt-3">
+          <h1 className="gradient-bg mb-4 md:mr-4 md:mt-2">
             and build them with
           </h1>
           <ul className="list-reset flex flex-col text-2xl py-3 px-4 border-grey-darkest border-solid border-2">
