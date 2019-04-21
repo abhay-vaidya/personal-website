df906c838b7d94729536fd3aa81d2d02ee496d82
abhay-vaidya <abhay6547@gmail.com>
2019-04-21 11:40:45 -0400

    Extend default font sizes

--- src/containers/Section.jsx
+++ src/containers/Section.jsx
@@ -11,7 +11,7 @@ class Section extends Component {
           className="p-12 flex flex-col md:flex-row w-full items-center justify-end"
         >
           <div className="w-full md:w-1/6 self-start">
-            <h1 className="text-black section-title text-5xl mb-5 md:mb-0">
+            <h1 className="text-black section-title text-5xl md:text-7xl lg:text-8xl mb-5 md:mb-0">
               {title}
             </h1>
           </div>
