de2ba91424f72ca18a3dc5c2a8f34c680eb3d1bb
abhay-vaidya <abhay6547@gmail.com>
2019-04-21 11:51:00 -0400

    Add fade-in animation

--- src/containers/Section.jsx
+++ src/containers/Section.jsx
@@ -1,25 +1,28 @@
 import React, { Component } from 'react'
+import Fade from 'react-reveal/Fade'
 
 class Section extends Component {
   render() {
     const { title, id, children } = this.props
     return (
-      <div>
-        <hr className="border border-black" />
-        <section
-          id={id}
-          className="p-12 flex flex-col md:flex-row w-full items-center justify-end"
-        >
-          <div className="w-full md:w-1/6 self-start">
-            <h1 className="text-black section-title text-5xl md:text-7xl lg:text-8xl mb-5 md:mb-0">
-              {title}
-            </h1>
-          </div>
-          <div className="w-full md:w-5/6 flex flex-col justify-end">
-            {children}
-          </div>
-        </section>
-      </div>
+      <Fade bottom>
+        <div>
+          <hr className="border border-black" />
+          <section
+            id={id}
+            className="p-12 flex flex-col md:flex-row w-full items-center justify-end"
+          >
+            <div className="w-full md:w-1/6 self-start">
+              <h1 className="text-black section-title text-5xl md:text-7xl lg:text-8xl mb-5 md:mb-0">
+                {title}
+              </h1>
+            </div>
+            <div className="w-full md:w-5/6 flex flex-col justify-end">
+              {children}
+            </div>
+          </section>
+        </div>
+      </Fade>
     )
   }
 }
