5081bb362744d0d62fcb393aee5ae86e83e4b666
abhay-vaidya <abhay6547@gmail.com>
2019-04-21 19:52:23 -0400

    Tweak landing

--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -6,10 +6,12 @@ class Landing extends Component {
     return (
       <section
         id="landing"
-        className="flex flex-col md:flex-row w-full h-screen items-center justify-center p-8"
+        className="flex flex-col md:flex-row w-full min-h-screen items-center justify-center p-8"
       >
         <div className="w-full mt-24 md:mt-0 md:w-1/3">
-          <h1 className="pb-6 text-5xl gradient-bg">Hi, I'm Abhay.</h1>
+          <h1 className="leading-tight pb-6 text-5xl gradient-bg">
+            Hi, I'm Abhay.
+          </h1>
           <p>
             I'm a third-year Computer Science student at the University of
             Toronto.
