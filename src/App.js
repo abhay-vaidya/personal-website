2c9c4e6ecbf3c21cfa4e05cf84fa1f534224cac8
abhay-vaidya <abhay6547@gmail.com>
2019-05-26 12:32:54 -0400

    Convert App to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Landing from './containers/Landing'
@@ -12,22 +12,20 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 library.add(faGithub, faLinkedin, faFileAlt)
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <main className="container mx-auto">
-          <Landing />
-          <Skills />
-          <Experience />
-          <Courses />
-          <Projects />
-        </main>
-        <Footer />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <main className="container mx-auto">
+        <Landing />
+        <Skills />
+        <Experience />
+        <Courses />
+        <Projects />
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
