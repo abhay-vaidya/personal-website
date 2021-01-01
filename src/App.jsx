e71ed9d534482699aac875b969695df791d797aa
abhay-vaidya <abhay6547@gmail.com>
2021-01-01 12:35:44 -0500

    Add more padding to bottom on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { NavBar, Footer } from "./components";
 import { Courses, Experience, Landing, Projects, Skills } from "./containers";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faHubspot
+} from "@fortawesome/free-brands-svg-icons";
 import {
   faFileAlt,
   faExternalLinkAlt
 } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-library.add(faGithub, faLinkedin, faFileAlt, faExternalLinkAlt);
+library.add(faHubspot, faGithub, faLinkedin, faFileAlt, faExternalLinkAlt);
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <main className="container mx-auto">
+      <main className="container mx-auto mb-10 sm:mb-0">
         <Landing />
         <Skills />
         <Experience />
